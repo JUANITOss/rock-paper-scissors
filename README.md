15e353896de94bf982b71e9efd19266576bf9954
JUANITOss <0juansuazo0@gmail.com>
2022-12-17 19:21:34 -0300

    Upgraded README.md, improved html and completed getComputerChoice

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # rock-paper-scissors
+
+This project is a on-process development of the game known as "rock, paper and scissors". 
+
+## First version
+
+> Only works in the console.
---
+++ js/index.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    
+    let choices = ["rock","paper","scissors"];
+
+    let index = Math.floor(Math.random() * choices.length);
+    
+    return choices[index];
+}
+
+
+console.log(getComputerChoice())
